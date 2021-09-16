6d8636ed9a5784725ca50bff951025245b344d0e
Oliver Nunez <oliverync@gmail.com>
2021-09-16 16:57:11 -0400

    Primer commit

--- Controllers/GenerosController.cs
+++ Controllers/GenerosController.cs
@@ -8,29 +8,46 @@ using System.Threading.Tasks;
 
 namespace PeliculasAPI.Controllers
 {
-    [Route("api/genero")]
+    [Route("api/generos")]
     public class GenerosController
     {
+
         public IRepositorio _repositorio;
         public GenerosController(IRepositorio repositorio)
         {
             _repositorio = repositorio;
         }
 
-        [HttpGet]
+        [HttpGet]   // api/generos
+        [HttpGet("listado")]   // api/generos/listado
+        [HttpGet("/listado")]   // /listado
         public List<Genero> Get() {
 
             var generos = _repositorio.ObtenerTodosLosGeneros();
 
             return generos;
         }
-        
-        //[HttpGet]
-        //public Genero Get(int Id) {
 
-        //    var generos = _repositorio.ObtenerGeneroPorId(Id);
-        //    return generos;
-        //}
+        [HttpGet("{Id}")]   // api/generos/1
+        [HttpGet("GetGeneroPorId")]   // api/generos/GetGeneroPorId?id=1
+        public Genero Get(int Id)
+        {
+
+            var generos = _repositorio.ObtenerGeneroPorId(Id);
+            return generos;
+        }
+        
+        [HttpGet("{Id:int}/{nombre=null}")]   // api/generos/1/oliver
+        public Genero Get(int Id, string nombre)
+        {
+
+            var generos = _repositorio.ObtenerGeneroPorId(Id);
+
+            if (generos == null) {
+                //return NotFound();
+            }
+            return generos;
+        }
 
         [HttpPost]
         public void Post() { 
